e7d7f94249c2baf418a15c8d8847995de5f60508
VRaj361 <vrajnpatel16@gmail.com>
2022-03-18 22:40:35 +0530

    Fragment With ListView

--- src/main/java/com/myfirstapplication/NavigationBarBasic.java
+++ src/main/java/com/myfirstapplication/NavigationBarBasic.java
@@ -1,5 +1,6 @@
 package com.myfirstapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,8 +12,10 @@ import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -28,42 +31,84 @@ public class NavigationBarBasic extends AppCompatActivity {
     DrawerLayout drawerLayout;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_bar_basic);
 
-        toolbar=findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        navigationView=findViewById(R.id.nav_view);
-        drawerLayout=findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        navigationView = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-
+        fragment1_basic1 basic1 = new fragment1_basic1();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frame, basic1);
+        fragmentTransaction.commit();
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id=item.getItemId();
-                if(id==R.id.nav_home){
+                int id = item.getItemId();
+
+                if (id == R.id.nav_home) {
                     Toast.makeText(NavigationBarBasic.this, "Home", Toast.LENGTH_SHORT).show();
                     toolbar.setTitle("Home");
-                }else if(id==R.id.nav_gallery){
+                    fragment1_basic1 basic1 = new fragment1_basic1();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(R.id.frame, basic1);
+                    fragmentTransaction.commit();
+
+                } else if (id == R.id.nav_gallery) {
                     Toast.makeText(NavigationBarBasic.this, "Gallery", Toast.LENGTH_SHORT).show();
                     toolbar.setTitle("Gallery");
-                }else if(id==R.id.nav_slideshow){
+                    fragment2_basic2 basic2 = new fragment2_basic2();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(R.id.frame, basic2);
+                    fragmentTransaction.commit();
+                } else if (id == R.id.nav_slideshow) {
                     Toast.makeText(NavigationBarBasic.this, "Slideshow", Toast.LENGTH_SHORT).show();
                     toolbar.setTitle("Slideshow");
                 }
                 drawerLayout.closeDrawer(GravityCompat.START);
 
 
-
                 return true;
             }
         });
+    }
 
+    @Override
+    public void onBackPressed() {
+        fragment1_basic1 basic1 = new fragment1_basic1();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frame, basic1);
+        fragmentTransaction.commit();
+        toolbar.setTitle("Home");
+
+        AlertDialog.Builder build=new AlertDialog.Builder(this);
+//        because it is one override file
+        build.setIcon(R.drawable.bg_home_ig);
+        build.setTitle(getResources().getString(R.string.app_name));
+        build.setMessage("Please Click the name");
+        build.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        build.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(NavigationBarBasic.this,"no is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        build.show();
 
 
     }
--- src/main/java/com/myfirstapplication/fragment1_basic1.java
+++ src/main/java/com/myfirstapplication/fragment1_basic1.java
@@ -9,14 +9,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class fragment1_basic1 extends Fragment {
-
+    ListView listView;
+    String strLang[]={"Python","Java","cpp","go lang","c","android"};
+    int strLangImg[]={R.drawable.ic_python,R.drawable.ic_java,R.drawable.ic_c,R.drawable.ic_c,R.drawable.ic_house,R.mipmap.ic_launcher_round};
+    ArrayList<LangModel> arr;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootview=inflater.inflate(R.layout.fragment1_basic,container,false);
+
+        listView=rootview.findViewById(R.id.list_view_list);
+        arr=new ArrayList<LangModel>();
+        for(int i=0;i<strLang.length;i++){
+            LangModel langModel=new LangModel(strLang[i],strLangImg[i]);
+            arr.add(langModel);
+        }
+        MyBaseAdapter myBaseAdapter=new MyBaseAdapter(getContext(),arr);
+        listView.setAdapter(myBaseAdapter);
         return rootview;
     }
 }
